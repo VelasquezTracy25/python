8b6573481c1a15e5ed73bdecac4922ba34747b8a
Tracy Velasquez <tracyvelasquez@Tracys-MacBook-Pro.local>
2023-08-09 11:11:16 -0500

    list complete

---
+++ HelloWorld/datastructures/lists.py
@@ -0,0 +1,28 @@
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0]
+
+zeroes = [0] * 5
+combined = zeroes + letters
+
+
+rangeList = list(range(1, 100))
+
+chars = list("Hello World")
+
+print(combined)
+print(rangeList)
+print(chars)
+print(len(chars))
+
+print(letters[0])
+print(letters[-1])
+print(letters[0:2])
+print(letters[::2])
+
+
+newletters = ["a", "b", "c", "d"]
+letters[0] = "A"
+
+print(letters[-1])
+print(letters[:3])
