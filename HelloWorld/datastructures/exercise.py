b589eaf7cea6d3fd8a1c0c05c776f97f2d656284
Tracy Velasquez <tracyvelasquez@Tracys-MacBook-Pro.local>
2023-08-28 14:20:52 -0500

    get the most repeated char in a sentence

---
+++ HelloWorld/datastructures/exercise.py
@@ -0,0 +1,25 @@
+sentence = "This is a common interview question"
+
+# Find the most repeate character in the sentence
+
+# Questions to ask: Are capitalized chars and spaces counted?
+
+# go through each one and maybe get a count of the letters, then see which count is the highest
+# Guess is maybe use a dictionary and have them sorted by greatest to least
+
+char_freq = {}
+
+for char in sentence:  # loop through and make a dictionary of counts
+
+    if char in char_freq:
+        char_freq[char] += 1
+    else:
+        char_freq[char] = 1
+
+char_freq_sorted = sorted(  # sort all key values pairs as tuples, biggest first
+
+    char_freq.items(),
+    key=lambda kv: kv[1],
+    reverse=True)
+
+print(char_freq_sorted[0])  # return the first item of the sorts list
