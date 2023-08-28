d5a32ce077be409f559d5b46e566ad5120878bb7
Tracy Velasquez <tracyvelasquez@Tracys-MacBook-Pro.local>
2023-08-28 14:24:19 -0500

    try again

--- HelloWorld/datastructures/exercise.py
+++ HelloWorld/datastructures/exercise.py
@@ -2,7 +2,7 @@ sentence = "This is a common interview question"
 
 # Find the most repeate character in the sentence
 
-# Questions to ask: Are capitalized chars and spaces counted?
+# Questions to ask: Are capitalized chars and spaces counted? Should they be lower case.
 
 # go through each one and maybe get a count of the letters, then see which count is the highest
 # Guess is maybe use a dictionary and have them sorted by greatest to least
@@ -22,4 +22,5 @@ char_freq_sorted = sorted(  # sort all key values pairs as tuples, biggest first
     key=lambda kv: kv[1],
     reverse=True)
 
-print(char_freq_sorted[0])  # return the first item of the sorts list
+# return the first item of the sorts list (the char 'i')
+print(char_freq_sorted[0])
